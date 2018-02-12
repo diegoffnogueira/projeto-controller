26455eaa948616d9c1fd9b6f2687f3df00b173ed
Diego Nogueira <diegof.fnogueira@gmail.com>
2018-02-11 23:33:10 -0200

    Criando o controller de exemplo com parametro

--- src/main/java/com/diego/api/Application.java
+++ src/main/java/com/diego/api/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ControllerSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControllerSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
